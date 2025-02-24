68fac8950e8afd1f800a626096cbe1ccbacf3a44
ttetricas <estermeireles89@gmail.com>
2025-02-24 00:25:39 -0300

    inclui humor discutivel

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Olá querido usuário, por favor pressione uma tecla");
+Console.ReadKey();
+Console.WriteLine("Muito obrigada! MAS ESPERE!! antes que você se vá, queria dizer só uma coisa... boa noite *voz do Willian Bonner*");
